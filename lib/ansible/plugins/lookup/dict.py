271e3f4f0584299cd63fe4ca0c17991d0f043fc9
simonLeary42 <71396965+simonLeary42@users.noreply.github.com>
2024-12-17 13:24:24 -0500

    include type in error message

--- lib/ansible/plugins/lookup/dict.py
+++ lib/ansible/plugins/lookup/dict.py
@@ -70,7 +70,7 @@ class LookupModule(LookupBase):
         for term in terms:
             # Expect any type of Mapping, notably hostvars
             if not isinstance(term, Mapping):
-                raise AnsibleError(f"with_dict expects a dict, got {term}")
+                raise AnsibleError(f"with_dict expects a dict, got '{term}' of type {type(term)})")
 
             results.extend(self._flatten_hash_to_list(term))
         return results
