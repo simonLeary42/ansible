7edd8ce812eaa501267213376362424277223ed8
Simon Leary <simon.leary42@proton.me>
2024-12-27 12:53:29 -0500

    add get_options function for CallbackBase

--- lib/ansible/plugins/callback/__init__.py
+++ lib/ansible/plugins/callback/__init__.py
@@ -174,6 +174,9 @@ class CallbackBase(AnsiblePlugin):
     def get_option(self, k):
         return self._plugin_options[k]
 
+    def get_options(self):
+        return self._plugin_options
+
     def set_options(self, task_keys=None, var_options=None, direct=None):
         """ This is different than the normal plugin method as callbacks get called early and really don't accept keywords.
             Also _options was already taken for CLI args and callbacks use _plugin_options instead.
